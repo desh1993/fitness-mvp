a5c677d962c5ba0aa8607dc4bf688ed2ff8cdb98
desh1993 <szemingrage@gmail.com>
2026-01-13 13:44:09 +0800

    Added ziggy js to prevent error in digital ocean

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -2,8 +2,9 @@ import { createInertiaApp } from '@inertiajs/react';
 import createServer from '@inertiajs/react/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import ReactDOMServer from 'react-dom/server';
-import { RouteName } from 'ziggy-js';
-import { route } from '../../vendor/tightenco/ziggy';
+import type { RouteName } from 'ziggy-js';
+import { route } from 'ziggy-js';
+// import { route } from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
